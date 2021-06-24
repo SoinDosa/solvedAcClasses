9755b4c6df12f32d10663f1e150283bd2bc7b8bf
SoinDosa <rlatjsgnl519@gmail.com>
2021-06-24 17:25:22 +0900

    add

---
+++ CLASS1/1000.cpp
@@ -0,0 +1,13 @@
+// A+B
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+    if(a < 0 || a > 10 || b < 0 || b > 10)
+        return 0;
+    
+    cout << a+b;
+    return 0;
+}
