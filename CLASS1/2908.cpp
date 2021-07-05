6cb38d193b8a4ba2475c018472451c92aecf5e93
SoinDosa <rlatjsgnl519@gmail.com>
2021-07-05 15:50:58 +0900

    Update 2908.cpp

--- CLASS1/2908.cpp
+++ CLASS1/2908.cpp
@@ -1,14 +1,53 @@
+// 2908. 상수
 #include <iostream>
 using namespace std;
+
 int main() {
-    int a, b;
-    
-    cin >> a >> b;
-    
-    if(a < 0 || a > 10 || b < 0 || b > 10){
-        return 0;
-    }
-    
-    cout << a+b;
-    return 0;
+	char num1[4] = { 0, };
+	char num2[4] = { 0, };
+	char tmp;
+	do {
+		cin >> num1;
+	} while (num1[0] > '9' || num1[0] < '0' || num1[1] > '9' || num1[1] < '0' || num1[2] > '9' || num1[2] < '0');
+
+	do {
+		cin >> num2;
+	} while (num2[0] > '9' || num2[0] < '0' || num2[1] > '9' || num2[1] < '0' || num2[2] > '9' || num2[2] < '0');
+
+	tmp = num1[0];
+	num1[0] = num1[2];
+	num1[2] = tmp;
+
+	tmp = num2[0];
+	num2[0] = num2[2];
+	num2[2] = tmp;
+
+	if (num1[0] > num2[0]) {
+		cout << num1;
+	}
+	else if (num1[0] < num2[0]) {
+		cout << num2;
+	}
+	else {
+		if (num1[1] > num2[1]) {
+			cout << num1;
+		}
+		else if (num1[1] < num2[1]) {
+			cout << num2;
+		}
+		else {
+			if (num1[2] > num2[2]) {
+				cout << num1;
+			}
+			else if (num1[2] < num2[2]) {
+				cout << num2;
+			}
+			else {
+				cout << num1;
+			}
+		}
+	}
+
+	return 0;
+	
 }
